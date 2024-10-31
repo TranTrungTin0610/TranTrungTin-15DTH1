ee80e7d6b6898635ef36ec6a90857cb8014578b4
TranTrungTin0610 <trantrungtinhdbl@gmail.com>
2024-10-31 15:28:37 +0700

    trantrungtin

---
+++ app_basic/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AnimatedListExample(),
+    );
+  }
+}
+
+class AnimatedListExample extends StatefulWidget {
+  @override
+  _AnimatedListExampleState createState() => _AnimatedListExampleState();
+}
+
+class _AnimatedListExampleState extends State<AnimatedListExample> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  final List<String> _items = [];
+
+  void _addItem() {
+    final newIndex = _items.length;
+    _items.add('Item ${newIndex + 1}');
+    _listKey.currentState!.insertItem(newIndex);
+  }
+
+  void _removeItem(int index) {
+    final removedItem = _items[index];
+    _items.removeAt(index);
+    _listKey.currentState!.removeItem(
+      index,
+      (context, animation) => _buildItem(removedItem, animation),
+    );
+  }
+
+  Widget _buildItem(String item, Animation<double> animation) {
+    return FadeTransition(
+      opacity: animation,
+      child: ListTile(
+        title: Text(item),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () => _removeItem(_items.indexOf(item)),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedList Example'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: _addItem,
+          ),
+        ],
+      ),
+      body: AnimatedList(
+        key: _listKey,
+        initialItemCount: _items.length,
+        itemBuilder: (context, index, animation) {
+          return _buildItem(_items[index], animation);
+        },
+      ),
+    );
+  }
+}
